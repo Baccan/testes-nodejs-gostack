b54f37fc48fe848d4007e4354e878cfefe278a1d
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-07-28 18:02:55 -0300

    Gerando dados aleatórios e finalização do módulo

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -2,7 +2,8 @@ import request from 'supertest';
 import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
-import User from '../../src/app/models/User';
+// import User from '../../src/app/models/User';
+import factory from '../factories';
 import truncate from '../util/truncate';
 
 describe('User', () => {
@@ -11,10 +12,11 @@ describe('User', () => {
   });
 
   it('should encrypt user password when new user created', async () => {
-    const user = await User.create({
-      name: 'Gustavo Baccan',
-      email: 'gustavobaccang@gmail.com',
-      password: '123456',
+    // const user = await User.create({
+    const user = await factory.create('User', {
+      // name: 'Gustavo Baccan',
+      // email: 'gustavobaccang@gmail.com',
+      password: '123456', // caso tire o password ele gera um usuario com senha aleatoria
     });
 
     const compareHash = await bcrypt.compare('123456', user.password_hash);
@@ -23,33 +25,42 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    // Cria dados de um usuario aleatoriamente mas nao gera uma tabela no banco
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Gustavo Baccan',
-        email: 'gustavobaccang@gmail.com',
-        password: '123456',
-      });
+      .send(
+        user
+        // name: 'Gustavo Baccan',
+        // email: 'gustavobaccang@gmail.com',
+        // password: '123456',
+      );
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should be not able to register with duplicated email', async () => {
+    // Cria dados de um usuario aleatoriamente mas nao gera uma tabela no banco
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Gustavo Baccan',
-        email: 'gustavobaccang@gmail.com',
-        password: '123456',
-      });
+      .send(
+        user
+        // name: 'Gustavo Baccan',
+        // email: 'gustavobaccang@gmail.com',
+        // password: '123456',
+      );
 
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Gustavo Baccan',
-        email: 'gustavobaccang@gmail.com',
-        password: '123456',
-      });
+      .send(
+        user
+        // name: 'Gustavo Baccan',
+        // email: 'gustavobaccang@gmail.com',
+        // password: '123456',
+      );
 
     expect(response.status).toBe(400);
   });
