0e869a467452fa851fc21d6b034d9df605d24def
Gustavo Baccan Gomes <gustavobaccang@gmail.com>
2019-07-28 17:38:26 -0300

    Checagem de email duplicado

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -2,8 +2,16 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    if (req.body.email === 'gustavobaccango@gmail.com') {
-      return res.status(400).json({ error: 'E-mail ruim' });
+    // if (req.body.email === 'gustavobaccango@gmail.com') {
+    //   return res.status(400).json({ error: 'E-mail ruim' });
+    // }
+
+    const { email } = req.body;
+
+    const checkEmail = await User.findOne({ where: { email } });
+
+    if (checkEmail) {
+      return res.status(400).json({ error: 'Duplicated email' });
     }
 
     const user = await User.create(req.body);
